943b6d67ee3c55bb000a85c965f0d0a754917b0d
afiffauz1 <mz.afif28@gmail.com>
2019-08-01 17:01:16 +0700

    add guest activity functionality

--- app/src/main/java/com/example/pengendaliapi/debtbook/MainActivity.java
+++ app/src/main/java/com/example/pengendaliapi/debtbook/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.pengendaliapi.debtbook;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (currentUser == null){
+            sendUserToGuestActivity();
+        }
+    }
+
+    private void sendUserToGuestActivity(){
+        Intent intent = new Intent(MainActivity.this, GuestActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+    }
+
 }
+
