b0e599f63ca27f44449d974c685c47bdaf3410a2
Abyan <lionissinga@gmail.com>
2019-08-02 14:23:06 +0700

    Revised LoginActivity, added forgot password

--- app/src/main/java/com/example/pengendaliapi/debtbook/LoginActivity.java
+++ app/src/main/java/com/example/pengendaliapi/debtbook/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.pengendaliapi.debtbook;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -15,27 +16,29 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText etEmail, etPassword;
     Button bLoginButton;
+    TextView tvForgotPassword;
     FirebaseAuth fbAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         etEmail = (EditText)findViewById(R.id.email);
         etPassword = (EditText)findViewById(R.id.password);
         bLoginButton = (Button)findViewById(R.id.loginButton);
+        tvForgotPassword = (TextView)findViewById(R.id.forgotPassword);
         fbAuth = FirebaseAuth.getInstance();
 
         bLoginButton.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +51,25 @@ public class LoginActivity extends AppCompatActivity {
                     login();
                 } else {
                     Toast.makeText(LoginActivity.this, "Please fill in all fields",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        tvForgotPassword.setPaintFlags(tvForgotPassword.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        tvForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!etEmail.getText().toString().isEmpty()) {
+                    fbAuth.getInstance().sendPasswordResetEmail(etEmail.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Toast.makeText(LoginActivity.this, "Email sent",Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                } else {
+                    Toast.makeText(LoginActivity.this, "Please insert your email    ",Toast.LENGTH_SHORT).show();
                 }
             }
         });
