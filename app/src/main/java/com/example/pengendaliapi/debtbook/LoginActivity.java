666e2ddb564139ebb79deb898ac44fbaa9044e39
Abyan <lionissinga@gmail.com>
2019-08-02 13:52:06 +0700

    Added intents to LoginActivity and RegisterActivity from GuestActivity

--- app/src/main/java/com/example/pengendaliapi/debtbook/LoginActivity.java
+++ app/src/main/java/com/example/pengendaliapi/debtbook/LoginActivity.java
@@ -2,15 +2,29 @@ package com.example.pengendaliapi.debtbook;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText etEmail, etPassword;
+    Button bLoginButton;
+    FirebaseAuth fbAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +33,38 @@ public class LoginActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        etEmail = (EditText)findViewById(R.id.email);
+        etPassword = (EditText)findViewById(R.id.password);
+        bLoginButton = (Button)findViewById(R.id.loginButton);
+        fbAuth = FirebaseAuth.getInstance();
+
+        bLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                String email = etEmail.getText().toString();
+                String password = etPassword.getText().toString();
+
+                if (!email.isEmpty() && !password.isEmpty()) {
+                    login();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Please fill in all fields",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void login() {
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        fbAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Login success!",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Please recheck your email or password",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
