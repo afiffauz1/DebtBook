2dc7aaa7c9eb43eb1d5c2b634c7632b5efee0f26
Abyan <lionissinga@gmail.com>
2019-08-02 10:42:30 +0700

    Added intents to LoginActivity and RegisterActivity from GuestActivity

--- app/src/main/java/com/example/pengendaliapi/debtbook/GuestActivity.java
+++ app/src/main/java/com/example/pengendaliapi/debtbook/GuestActivity.java
@@ -2,13 +2,46 @@ package com.example.pengendaliapi.debtbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class GuestActivity extends AppCompatActivity {
+
+    Button btnLogin, btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest);
+
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnRegister = (Button)findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goLogin();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goRegister();
+            }
+        });
+    }
+
+    private void goLogin() {
+        Intent login = new Intent(GuestActivity.this, LoginActivity.class);
+        startActivity(login);
+    }
+
+    private void goRegister() {
+        Intent register = new Intent(GuestActivity.this, RegisterActivity.class);
+        startActivity(register);
     }
 }
