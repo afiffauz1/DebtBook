8c5c3d2677682d260e30feb8ac564798de31d1f3
Abyan <lionissinga@gmail.com>
2019-08-07 13:00:19 +0700

    Revised LoginActivity, can login with either e-mail or username

--- app/src/main/java/com/example/pengendaliapi/debtbook/LoginActivity.java
+++ app/src/main/java/com/example/pengendaliapi/debtbook/LoginActivity.java
@@ -29,7 +29,7 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText etEmail, etPassword;
+    EditText etUser, etPassword;
     Button bLoginButton;
     TextView tvForgotPassword;
     FirebaseAuth fbAuth;
@@ -40,7 +40,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        etEmail = (EditText)findViewById(R.id.email);
+        etUser = (EditText)findViewById(R.id.user);
         etPassword = (EditText)findViewById(R.id.password);
         bLoginButton = (Button)findViewById(R.id.loginButton);
         tvForgotPassword = (TextView)findViewById(R.id.forgotPassword);
@@ -50,14 +50,13 @@ public class LoginActivity extends AppCompatActivity {
         bLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = etEmail.getText().toString();
+                String user = etUser.getText().toString();
                 String password = etPassword.getText().toString();
 
-                if (!email.isEmpty() && !password.isEmpty()) {
+                if (!user.isEmpty() && !password.isEmpty()) {
                     login();
-//                    loginWithUsername();
                 } else {
-                    Toast.makeText(LoginActivity.this, "Please fill in all fields",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -67,71 +66,62 @@ public class LoginActivity extends AppCompatActivity {
         tvForgotPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!etEmail.getText().toString().isEmpty()) {
-                    fbAuth.getInstance().sendPasswordResetEmail(etEmail.getText().toString())
-                            .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    Toast.makeText(LoginActivity.this, "Email sent",Toast.LENGTH_SHORT).show();
-                                }
-                            });
+                if (!etUser.getText().toString().isEmpty()) {
+                    fbAuth.getInstance().sendPasswordResetEmail(etUser.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Toast.makeText(LoginActivity.this, "E-mail sent", Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 } else {
-                    Toast.makeText(LoginActivity.this, "Please insert your email    ",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Please insert your e-mail", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
     private void login() {
-        String email = etEmail.getText().toString();
-        String password = etPassword.getText().toString();
+        final String email = etUser.getText().toString();
+        final String username = etUser.getText().toString();
+        final String password = etPassword.getText().toString();
 
         fbAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
                     sendUserToDashboardActivity();
-                    Toast.makeText(LoginActivity.this, "Login success!",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Login success!", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(LoginActivity.this, "Please recheck your email or password",Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
+                    fbDatabase.getReference().child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            for (DataSnapshot uidSnapshot : dataSnapshot.getChildren()) {
+                                String un = uidSnapshot.child("username").getValue().toString();
 
-    private void loginWithUsername() {
-        final String username = etEmail.getText().toString();
-        final String password = etPassword.getText().toString();
+                                if (un.equals(username)) {
+                                    String email = uidSnapshot.child("email").getValue().toString();
 
-        fbDatabase.getReference().child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for (DataSnapshot uidSnapshot : dataSnapshot.getChildren()) {
-                    String un = uidSnapshot.child("username").getValue().toString();
-
-                    if (un.equals(username)) {
-                        String email = uidSnapshot.child("email").getValue().toString();
-
-                        Toast.makeText(LoginActivity.this, email,Toast.LENGTH_SHORT).show();
-
-                        fbAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    sendUserToDashboardActivity();
-                                    Toast.makeText(LoginActivity.this, "Login success!",Toast.LENGTH_SHORT).show();
-                                } else {
-                                    Toast.makeText(LoginActivity.this, "Please recheck your email or password",Toast.LENGTH_SHORT).show();
+                                    fbAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<AuthResult> task) {
+                                            if (task.isSuccessful()) {
+                                                sendUserToDashboardActivity();
+                                                Toast.makeText(LoginActivity.this, "Login success!", Toast.LENGTH_SHORT).show();
+                                            } else {
+                                                Toast.makeText(LoginActivity.this, "Please recheck your email or password", Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
                                 }
                             }
-                        });
-                    }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
                 }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
             }
         });
     }
